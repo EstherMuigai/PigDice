6832497c43551571a709a3347d23790e43a715ce
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-03-01 16:55:10 +0300

    Add function to display points

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,19 @@ $("document").ready (function(){
     }); */
     $("div#rolldice").click(function(){
         event.preventDefault();
-        $("div#rolldice").hide();
-        $("div#dice").show();
-        rolling();
+        $("div#rolldice").hide().show(1000);
+        $("div#dice").show().delay(500).hide(500);
+        var points = rolling ();
+        displayPoints(points);
+
     });
 });
 
 //BUSINESS LOGIC
 function rolling(number) {
-    number = Math.floor(Math.random() * 6);
-    $("ul#pointsA").append("<li><span class='point'>" +number+ "</span></li>");
+    return Math.floor(Math.random() * 6);
+}
+
+function displayPoints(number2) {
+    $("ul#pointsA").append("<li><span class='pointsA'>" +number2+ "</span></li>");
 }
