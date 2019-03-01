33f7b16d3a94e7b01cd6033cdae56183c0cae616
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-03-01 16:17:18 +0300

    Add random rolling function

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,16 @@ $("document").ready (function(){
             $("div#welcome").hide(); 
         });
     }); */
+    $("div#rolldice").click(function(){
+        event.preventDefault();
+        $("div#rolldice").hide();
+        $("div#dice").show();
+        rolling();
+    });
 });
+
+//BUSINESS LOGIC
+function rolling(number) {
+    number = Math.floor(Math.random() * 6);
+    $("ul#pointsA").append("<li><span class='point'>" +number+ "</span></li>");
+}
