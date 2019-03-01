652b56c8501582ce7fb4cda2aac4387141950327
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-03-01 15:14:06 +0300

    Add fadein fadeout functions

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//USER INTERFACE LOGIC
+$("document").ready (function(){
+   /** $("div#welcome").fadeIn("slow",function(){
+        $("div#welcome").show();
+    });
+    $("div#welcome").click(function(){
+        $("div#welcome").fadeOut("slow",function(){
+            $("div#welcome").hide(); 
+        });
+    }); */
+});
